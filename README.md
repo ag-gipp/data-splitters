375d90bf2a98452e6471fab5eb1ed46e6d93c7c3
AndreG-P <andre.greiner-petter@t-online.de>
2019-12-02 15:55:58 +0900

    Update script and create README

---
+++ README.md
@@ -0,0 +1,19 @@
+The `splitter.sh` expects to arguments, the input and output folder.
+For example, if your data is in `data` and you want to put into `out`:
+```sh
+./splitter data out
+```
+
+The `data` directory should have the structure:
+```
+- data
+   |- dataset1
+   |    |- lang1
+   |    |- lang2
+   |- dataset2
+   |    |- lang1
+   |    |- lang2
+   . . .
+   . . .
+   . . .
+```
--- splitter.sh
+++ splitter.sh
@@ -11,36 +11,56 @@ MAXL=8
 HALFMAXL=$((MAXL/2))
 NUMFILES=2000
 
-for d in $DIRS; do
-	for id in $(ls $1/$d); do
-		FCOUNTER=0
-		
-		$(mkdir -p $2/$d/$id)
-		echo "Split $1/$d/$id"
-		
-		for f in $(ls $1/$d/$id); do
-			LINE=$(wc -l $1/$d/$id/$f | awk '{print $1}')
-			if [[ $LINE -ge $MINL ]] && [[ $LINE -le $MAXL ]] && [[ $FCOUNTER -lt $NUMFILES ]]; then
-				$(cp $1/$d/$id/$f $2/$d/$id/$f)
-				FCOUNTER=$((FCOUNTER+1))
-				#echo "Copied $id/$f"
-			fi
-		done
+for DATAD in $DIRS; do
+	LANGDIRS=$(ls $INPUT/$DATAD)
+	FIRST=$(echo $LANGDIRS | awk '{print $1}')
+	FCOUNTER=0
 
-		if [ $FCOUNTER -lt $NUMFILES ]; then
-			echo "Only copied $FCOUNTER files. Filling up the rest with longer files."
-			for f in $(ls $1/$d/$id); do
-				LINE=$(wc -l $1/$d/$id/$f | awk '{print $1}')
-				if [[ $LINE -gt $MAXL ]] && [[ $FCOUNTER -lt $NUMFILES ]]; then
-                                	SLINE=$((LINE/2 - HALFMAXL))
-                                	ELINE=$((SLINE + MAXL))
-                        	        $(sed -n "$SLINE,$ELINE p" $1/$d/$id/$f >> $2/$d/$id/$f)
-                	                FCOUNTER=$((FCOUNTER+1))
-					echo "Cut $id/$f lines $SLINE to $ELINE (total lines $LINE)"
-	                        fi
-			done
-		fi
-		echo "Done generating $FCOUNTER files in $1/$d/$id"
+	# create output DIRS
+	for OUTD in $LANGDIRS; do
+		$(mkdir -p $OUTPUT/$DATAD/$OUTD)
 	done
-done
 
+	echo "Processing $INPUT/$DATAD"
+
+	# iterate over all files in first data dir (often en)
+	for file in $(ls $INPUT/$DATAD/$FIRST); do
+		fileE=${file##*-}
+		fileID=${file%-$fileE}
+
+		# if line number is within specified range, copy the files
+		LINE=$(wc -l $INPUT/$DATAD/$FIRST/$file | awk '{print $1}')
+	  if [[ $LINE -ge $MINL ]] && [[ $LINE -le $MAXL ]] && [[ $FCOUNTER -lt $NUMFILES ]]; then
+			for lang in $LANGDIRS; do
+				fileN=$fileID-$lang.txt
+				$(cp $INPUT/$DATAD/$lang/$fileN $OUTPUT/$DATAD/$lang/)
+			done
+	    FCOUNTER=$((FCOUNTER+1))
+	  fi
+	done
+
+	echo "Copied $FCOUNTER files with $MINL and $MAXL lines in $INPUT/$DATAD"
+
+	if [ $FCOUNTER -lt $NUMFILES ]; then
+	  echo "Only copied $FCOUNTER files. Filling up the rest with longer files."
+	  for file in $(ls $INPUT/$DATAD/$FIRST); do
+			fileE=${file##*-}
+			fileID=${file%-$fileE}
+
+			# if line number is within specified range, copy the files
+			LINE=$(wc -l $INPUT/$DATAD/$FIRST/$file | awk '{print $1}')
+	    if (( LINE > MAXL )) &&  ((FCOUNTER < NUMFILES)); then
+        SLINE=$((LINE/2 - HALFMAXL + 1))
+        ELINE=$((SLINE + MAXL + 1))
+				for lang in $LANGDIRS; do
+					fileN=$fileID-$lang.txt
+					$(sed -n "$SLINE,$ELINE p" $INPUT/$DATAD/$lang/$fileN >> $OUTPUT/$DATAD/$lang/$fileN)
+				done
+        FCOUNTER=$((FCOUNTER+1))
+	      echo "Cut $fileID in $DATAD: lines $SLINE to $ELINE (total lines $LINE)"
+			fi
+	  done
+	fi
+
+	echo "Filled up longer files and reached $FCOUNTER."
+done
